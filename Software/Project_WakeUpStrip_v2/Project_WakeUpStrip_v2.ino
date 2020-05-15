02ed0ff02e14aa5b0f52941b8cd65fc8d6674a97
doublemint84 <nikitka.borodavko@gmail.com>
2020-05-16 00:20:35 +0300

    fixed main program
    -now tm1637 display shows real time

--- Software/Project_WakeUpStrip_v2/Project_WakeUpStrip_v2.ino
+++ Software/Project_WakeUpStrip_v2/Project_WakeUpStrip_v2.ino
@@ -1,51 +1,69 @@
+//----------------------НАСТРОЙКИ-----------------------
 #define CLK 9
 #define DT 10
+#define CLK_tm 7
+#define DIO 6
 #define SW 11
 #define maxArrSize 30
 #define kolArr 10
-
+#define oled_adr 0x78 
+#define clock_adr 0x68
+//---------------------БИБЛИОТЕКИ
 #include <OLED_I2C.h>
-
+#include "DS3231.h"
 #include "GyverEncoder.h"
-//Encoder enc1(CLK, DT);      // для работы без кнопки
+#include <Wire.h>
+#include "GyverTM1637.h"
+
 Encoder enc1(CLK, DT, SW);  // для работы c кнопкой
 OLED  myOLED(SDA, SCL);
+RTClib rtc;
+GyverTM1637 disp(CLK_tm, DIO);
+
 extern uint8_t SmallFont[];
-//Encoder enc1(CLK, DT, SW, TYPE2);  // для работы c кнопкой и сразу выбираем тип
-//Encoder enc1(CLK, DT, ENC_NO_BUTTON, TYPE2);  // для работы без кнопки и сразу выбираем тип
 
 int value = 0, change = 0;
+bool inMenu = false;
+DateTime t_now, t_prev;
 
 char menu[][maxArrSize] = {"Settings", "Alarm1(--:--, OFF)", "Alarm2(--:--, OFF)", "Light", "BACK", "Sms", "Connection", "Reset", "Somebody", "once told me"};
 
 void setup() {
+  Wire.begin();
+  t_now = rtc.now();
+  t_prev = t_now;
+  disp.clear();
+  disp.brightness(7);
+  disp.displayClock(byte(t_now.hour()), byte(t_now.minute()));
   if (!myOLED.begin(SSD1306_128X64))
     while (1);  // In case the library failed to allocate enough RAM for the display buffer...
 
   myOLED.setFont(SmallFont);
   enc1.setType(TYPE2);    // тип энкодера TYPE1 одношаговый, TYPE2 двухшаговый. Если ваш энкодер работает странно, смените тип
   myOLED.clrScr();
-  myOLED.print(F("Main menu"), LEFT, 0);
+ /* myOLED.print(F("Main menu"), LEFT, 0);
   for (int i = 0; i < ((kolArr - change > 6) ? 6 : kolArr - change); i++) {
     if (i == 0) {
       myOLED.print(String("->") + String(menu[change + i]), LEFT, 8 * (i + 1));
     } else {
       myOLED.print(String("  ") + String(menu[change + i]), LEFT, 8 * (i + 1));
     }
-  }
+  }*/
   myOLED.update();
 
 }
 
 void loop() {
-  // обязательная функция отработки. Должна постоянно опрашиваться
-  enc1.tick();
-
-
-
-  if (enc1.isTurn()) {       // если был совершён поворот (индикатор поворота в любую сторону)
-    if (enc1.isLeft() and (change > 0)) change--;
-    if (enc1.isRight() and (change < kolArr - 1)) change++;
+  inputTick();
+  //setupTick();
+  t_now = rtc.now();
+  if (t_now.minute() != t_prev.minute()){
+    disp.displayClock(byte(t_now.hour()), byte(t_now.minute()));
+    t_prev = t_now;
+  }
+  
+  /*if (enc1.isTurn()) {       // если был совершён поворот (индикатор поворота в любую сторону)
+    
 
     myOLED.clrScr();
     myOLED.print(F("Main menu"), LEFT, 0);
@@ -57,5 +75,13 @@ void loop() {
       }
     }
     myOLED.update();
+  }*/
+}
+
+void inputTick(){
+  enc1.tick();
+  if (inMenu) {
+    if (enc1.isLeft() and (change > 0)) change--;
+    if (enc1.isRight() and (change < kolArr - 1)) change++;
   }
 }
