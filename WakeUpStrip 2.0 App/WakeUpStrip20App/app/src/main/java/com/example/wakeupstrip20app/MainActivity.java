b8e4a260798d95a7287f9790e35970eb9f376db9
doublemint84 <nikitka.borodavko@gmail.com>
2020-06-06 02:05:13 +0300

    Added fragment switch to app +List in Connect

--- WakeUpStrip 2.0 App/WakeUpStrip20App/app/src/main/java/com/example/wakeupstrip20app/MainActivity.java
+++ WakeUpStrip 2.0 App/WakeUpStrip20App/app/src/main/java/com/example/wakeupstrip20app/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.wakeupstrip20app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ConnectFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()) {
+                case R.id.nav_connection:
+                    selectedFragment = new ConnectFragment();
+                    break;
+                case R.id.nav_alarms:
+                    selectedFragment = new AlarmsFragment();
+                    break;
+                case R.id.nav_lamp:
+                    selectedFragment = new LampFragment();
+                    break;
+                case R.id.nav_device_settings:
+                    selectedFragment = new DeviceSettingsFragment();
+                    break;
+                case R.id.nav_app_settings:
+                    selectedFragment = new AppSettingsFragment();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            return true;
+        }
+    };
 }
