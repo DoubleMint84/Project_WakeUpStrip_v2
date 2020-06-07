5ed6d3897a4812d36578ee9ae05058edd8788dfc
doublemint84 <nikitka.borodavko@gmail.com>
2020-06-07 16:14:35 +0300

    Added bluetooth paired list WARNING: Bug with button in fragment_connect

--- WakeUpStrip 2.0 App/WakeUpStrip20App/app/src/main/java/com/example/wakeupstrip20app/ConnectFragment.java
+++ WakeUpStrip 2.0 App/WakeUpStrip20App/app/src/main/java/com/example/wakeupstrip20app/ConnectFragment.java
@@ -1,18 +1,59 @@
 package com.example.wakeupstrip20app;
 
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+import java.util.Set;
+import static android.R.layout.simple_list_item_1;
+
 public class ConnectFragment extends Fragment {
+    private TextView textInfo;
+    ListView listViewPairedDevice;
+    Button bSetup;
+    ArrayList<String> pairedDeviceArrayList;
+    ArrayAdapter pairedDeviceAdapter;
+
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_connect, container, false);
+    public View onCreateView(@NonNull final LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_connect, container, false);
+        textInfo = v.findViewById(R.id.textInfoPhone);
+        bSetup = v.findViewById(R.id.butSetup);
+        bSetup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Set<BluetoothDevice> pairedDevices = MainActivity.bluetoothAdapter.getBondedDevices();
+                if (pairedDevices.size() > 0) {
+                    pairedDeviceArrayList = new ArrayList<>();
+                    for (BluetoothDevice device : pairedDevices) {
+                        pairedDeviceArrayList.add(device.getName() + "\n" + device.getAddress());
+                    }
+                    pairedDeviceAdapter = new ArrayAdapter<>(inflater.getContext(), simple_list_item_1, pairedDeviceArrayList);
+                    listViewPairedDevice.setAdapter(pairedDeviceAdapter);
+                }
+            }
+        });
+        textInfo.setText(String.format("Это устройство: %s", MainActivity.bluetoothAdapter.getName() + " " + MainActivity.bluetoothAdapter.getAddress()));
+        listViewPairedDevice = (ListView)v.findViewById(R.id.listDevices);
+        return v;
     }
+
+
 }
--- WakeUpStrip 2.0 App/WakeUpStrip20App/app/src/main/java/com/example/wakeupstrip20app/MainActivity.java
+++ WakeUpStrip 2.0 App/WakeUpStrip20App/app/src/main/java/com/example/wakeupstrip20app/MainActivity.java
@@ -1,25 +1,77 @@
 package com.example.wakeupstrip20app;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.ArrayList;
+import java.util.UUID;
+
+import static android.R.layout.simple_list_item_1;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static BluetoothAdapter bluetoothAdapter;
+    public UUID myUUID;
+    public final String UUID_STRING_WELL_KNOWN_SPP = "00001101-0000-1000-8000-00805F9B34FB";
+    private static final int REQUEST_ENABLE_BT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH)){
+            Toast.makeText(this, "BLUETOOTH NOT support", Toast.LENGTH_LONG).show();
+            //finish();
+            return;
+        }
+
+        myUUID = UUID.fromString(UUID_STRING_WELL_KNOWN_SPP);
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            Toast.makeText(this, "Bluetooth is not supported on this hardware platform", Toast.LENGTH_LONG).show();
+            //finish();
+            return;
+        }
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ConnectFragment()).commit();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_ENABLE_BT) { // Если разрешили включить Bluetooth, тогда void setup()
+            if (resultCode == Activity.RESULT_OK) {
+                Toast.makeText(this, "BlueTooth включен, нажмите кнопку для открытия списка девайсов", Toast.LENGTH_SHORT).show();
+            } else { // Если не разрешили, тогда закрываем приложение
+                Toast.makeText(this, "BlueTooth не включён", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
